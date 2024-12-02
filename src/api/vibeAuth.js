db576819dd2a4ade5816410195b290a7b2a946b9
Akshay Mugale <akshay.mugale@lockated.com>
2024-12-03 00:37:09 +0530

    git api

---
+++ src/api/vibeAuth.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { getItemInLocalStorage } from "../utils/localStorage";
+
+const vibeAuth= axios.create({
+  // baseURL: "https://vibecopilot.ai",
+     baseURL: "https://app.myciti.life",
+
+});
+
+vibeAuth.interceptors.request.use(
+    (config) => {
+      const token = getItemInLocalStorage("VIBETOKEN");
+      
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+export default vibeAuth;
