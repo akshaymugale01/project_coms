773af979b68567742443efb325b0e276437a8552
Akshay Mugale <139070784+akshaymugale01@users.noreply.github.com>
2025-01-12 13:17:55 +0530

    NWW

---
+++ src/Extra/Demo.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import 'react-slideshow-image/dist/styles.css'
+
+
+
+const slideImages = [
+    {
+        url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        caption: 'Slide 1'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+        caption: 'Slide 2'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        caption: 'Slide 3'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        caption: 'Slide 1'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        caption: 'Slide 1'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        caption: 'Slide 1'
+    }
+];
+
+
+const Slideshow = () => {
+    return (
+        <div>
+             <SimpleImageSlider 
+                 width={896}
+                 height={504}
+                 images={slideImages}
+                 showBullets={true}
+                 showNavs={true}
+                 autoPlay={true}            
+                 autoPlayDelay={3.0} 
+                /> 
+        </div>
+    )
+}
+
+export default Slideshow;
