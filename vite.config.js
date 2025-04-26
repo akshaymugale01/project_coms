983f1ccd6701cac9cbecc5477c59dd880de0df1f
akshay <akshaymugale1@gnmail.com>
2025-04-26 12:21:19 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { builtinModules } from 'module'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       fs: 'fs',
     }
-  }
-})
+  },
+  optimizeDeps: {
+    exclude: ['node-fetch'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['node-fetch'],
+    },
+  },
+});
